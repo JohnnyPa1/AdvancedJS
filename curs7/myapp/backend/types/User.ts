e72d480ac8c1a812b972aaad2a3c0c676fac52b1
ZsoltSzilagyi1 <z_szilgyi@yahoo.com>
2024-06-12 13:05:34 +0300

    recapitulare

--- curs7/myapp/backend/types/User.ts
+++ curs7/myapp/backend/types/User.ts
@@ -9,7 +9,8 @@ export interface BasicUser {
     telefon: string,
     dataadaugare?:Date,
     inaltime?:number,
-    actiune?:string
+    actiune?:string,
+    poza?:string
 
   }
   export interface UserWithDetails extends BasicUser, User{
